bf3a5a582a3d5c127f9f8a82b7e26379b4098eec
hydrax117 <pauljoseph5000@gmail.com>
2025-09-30 12:51:33 +0100

    feat:pwa+landing page setup

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+import withPWA from "next-pwa";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const pwaConfig = {
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+  buildExcludes: [/app-build-manifest\.json$/],
+  sw: "sw.js",
 };
+
+const nextConfig = withPWA(pwaConfig)({
+  // ...other Next.js config options
+  fallbacks: {
+    document: "/offline", // Fallback for page requests
+  },
+});
 
 export default nextConfig;
